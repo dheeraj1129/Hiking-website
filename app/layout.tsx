814158856b586786ece47e41353bdfd63573317f
Your GitHub Username <Your GitHub Email>
2024-06-12 23:02:57 +0530

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +17,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        <main className="relative overflow-hidden"></main>
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
